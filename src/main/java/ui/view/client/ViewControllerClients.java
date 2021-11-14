226706405ef17d1dacfdfd7091acd332f103dea7
dmitriy.rashevskiy <dmitriy.rashevskiy@vizor-games.com>
2021-11-14 20:41:07 +0300

    Добавлено отображение простой информации о клиенте

--- src/main/java/ui/view/client/ViewControllerClients.java
+++ src/main/java/ui/view/client/ViewControllerClients.java
@@ -1,7 +1,9 @@
 package ui.view.client;
 
+import javafx.collections.ListChangeListener;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +18,22 @@ import javax.inject.Inject;
 public class ViewControllerClients implements ViewController {
     @FXML private TextField searchField;
     @FXML private ListView<Client> clientList;
+    @FXML private Pane clientInfoContainer;
+    @FXML private TextField surnameField;
+    @FXML private TextField nameField;
+    @FXML private TextField patronymicField;
+    @FXML private TextField phoneField;
 
     private final UiManager uiManager;
     private final IClientService clientService;
 
     @FXML
     private void initialize() {
+        clientList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        clientList.getSelectionModel().getSelectedItems().addListener((ListChangeListener<? super Client>) change -> {
+            change.next();
+            showClientInfo(change.getAddedSubList().get(0));
+        });
         clientList.setCellFactory(listView -> new ClientListCell());
         clientList.getItems().addAll(clientService.getClients());
     }
@@ -31,8 +43,15 @@ public class ViewControllerClients implements ViewController {
         return "view/client/clients.fxml";
     }
 
-    @FXML
     public void showNewClientDialog() {
         uiManager.openNewClientDialog();
+    }
+
+    public void showClientInfo(Client client) {
+        surnameField.setText(client.getSurname());
+        nameField.setText(client.getName());
+        patronymicField.setText(client.getPatronymic());
+        phoneField.setText(client.getPhone());
+        clientInfoContainer.setVisible(true);
     }
 }
